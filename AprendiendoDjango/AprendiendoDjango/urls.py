0a16a1b04a460a1331fe05a361c90f406e5860fd
yugosixs <yougozerosixs@gmail.com>
2025-05-28 11:54:22 -0600

    Actualiza rutas en urls.py

--- AprendiendoDjango/AprendiendoDjango/urls.py
+++ AprendiendoDjango/AprendiendoDjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('holamundo/', views.hola_Mundo, name="holamundindo")
 ]
+
